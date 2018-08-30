32d052fdbf40c9d43161e9ac7c8e2c467dcbb815
Raj <r.kadir@se16.qmul.ac.uk>
2018-08-30 16:00:58 +0100

    added Income Tax, National Insurance calculations

--- UKTaxCalculator/Program.cs
+++ UKTaxCalculator/Program.cs
@@ -19,13 +19,31 @@ namespace UKTaxCalculator
             Console.WriteLine("What is your yearly income? (all numbers no commas or spaces");
 
             // Store income from user
-            double income = Double.Parse(Console.ReadLine());
+            double grossIncome = Double.Parse(Console.ReadLine());
+
+            double taxable = CalculateTaxable(grossIncome);
+            double taxPercentage = CalculateTaxBands(grossIncome);
+            double taxPaid = taxable * taxPercentage;
+
+
+            double weeklyWage = grossIncome / 52;
+
+            double nationalInsurance = ((weeklyWage - 162) * 0.12) * 52;
+
 
             // Output income
-            Console.WriteLine("Total taxable is: " + CalculateTaxable(income) );
+            Console.WriteLine("Total taxable is: " + taxable );
+
+            Console.WriteLine("You fall under tax percentage: " + taxPercentage);
+
+            Console.WriteLine("Tax paid: " + taxPaid);
+            Console.WriteLine("National Insurance: " + nationalInsurance);
+
+            Console.WriteLine("You take home: " + CalculateNetIncome(grossIncome, taxPaid, nationalInsurance));
 
             // Wait for user to end program
             Console.ReadKey();
+
         }
 
         /**
@@ -34,13 +52,65 @@ namespace UKTaxCalculator
         static double CalculateTaxable(double amount)
         {
             // basic allowance which is not taxed
-            double basicPersonalAllowance = 11850.0;
+            double basicPersonalAllowance = 11859;
+            double taxable = amount;
 
-            double taxable = amount - basicPersonalAllowance;
-
-            // Now check which bands are applicable
+            // You don’t get a Personal Allowance on taxable income over £123,700.
+            if (amount < 123700)
+            {
+                taxable = amount - basicPersonalAllowance;
+            }
 
             return taxable;
         }
+
+
+        /**
+         * Calculate tax bands 
+         * */
+         static double CalculateTaxBands(double amount)
+         {
+            double percentageTax = 0;
+            /*
+             * Personal Allowance  Up to £11,850   0 %
+             * Basic rate  £11,851 to £46,350  20 %
+             * Higher rate £46,351 to £150,000 40 %
+             * Additional rate over £150,000   45 %
+             */
+
+            if(amount >= 11851 && amount <= 46350)
+            {
+                percentageTax = 0.20;
+            }else if(amount >= 46351 && amount <= 150000)
+            {
+                percentageTax = 0.40;
+            }else if(amount > 150000)
+            {
+                percentageTax = 0.45;
+            }
+
+            return percentageTax;
+         }
+
+         static double CalculateNetIncome(double grossIncome, double taxPaid, double nationalInsuarance)
+         {
+            /*
+            double basicPersonalAllowance = 11850.0;
+            double netIncome = 0;
+            double taxed = taxableAmount * taxPercentage;
+
+            if( (taxableAmount + basicPersonalAllowance) < 123700)
+            {
+                netIncome = taxableAmount - (taxed) + basicPersonalAllowance;
+            }
+            else
+            {
+                netIncome = taxableAmount - (taxed);
+            }*/
+
+            return grossIncome - taxPaid - nationalInsuarance;
+         }
     }
+
+
 }
