f1ce0265992aa0796f2cc3db9c14e7ceff73b523
Raj <32778886+rk16449@users.noreply.github.com>
2018-08-30 14:38:37 +0100

    added files manually through tortoise rather than visual studio

---
+++ UKTaxCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UKTaxCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String interpolation
+            string version = "0.0.1";
+            string author = "Raj";
+            string email = "raj.nry.k@gmail.com";
+
+            // Intro text
+            Console.WriteLine("Welcome to my UK tax calculator ");
+            Console.WriteLine($"author: {author}, version: {version}, email: {email} \n");
+
+            // Ask for income
+            Console.WriteLine("What is your yearly income? (all numbers no commas or spaces");
+
+            // Store income from user
+            double income = Double.Parse(Console.ReadLine());
+
+            // Output income
+            Console.WriteLine("Your total taxable amount is: " + calculateTaxable(income) + " tax");
+
+            // Wait for user to end program
+            Console.ReadKey();
+        }
+
+        /**
+         * Calculates the amount taxable
+         **/
+        static double calculateTaxable(double amount)
+        {
+            // basic allowance which is not taxed
+            double basicPersonalAllowance = 11850.0;
+
+            double taxable = amount - basicPersonalAllowance;
+
+            // Now check which bands are applicable
+
+            return taxable;
+        }
+    }
+}
